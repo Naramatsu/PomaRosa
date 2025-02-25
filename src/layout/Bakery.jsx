35ce7d8c4692802eeaaa11bc8dd74dd996de208e
Naramatsu <joncyy_19@hotmail.com>
2025-02-25 08:56:10 -0500

    add scrollTop behavior at change page

--- src/layout/Bakery.jsx
+++ src/layout/Bakery.jsx
@@ -32,6 +32,7 @@ const Bakery = () => {
 
   useEffect(() => {
     setTheme(BAKERY);
+    window.scrollTo(0, 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
--- src/layout/Kitchen.jsx
+++ src/layout/Kitchen.jsx
@@ -50,6 +50,7 @@ const Kitchen = () => {
 
   useEffect(() => {
     setTheme(KITCHEN);
+    window.scrollTo(0, 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
